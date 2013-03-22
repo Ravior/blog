c811253800741ab9d1a07f8f902c176b58197995
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2013-03-22 18:19:47 +0700

    font awesome

--- header.php
+++ header.php
@@ -1,10 +1,18 @@
 <!DOCTYPE html>
-<html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!-->
+<html lang="en">
+<!--<![endif]-->
 <head>
 	<meta http-equiv="content-type" content="text/html; charset=utf-8"0>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="<?php echo get_bloginfo("template_url")?>/bootstrap/css/bootstrap.min.css" type="text/css" media="screen" charset="utf-8"0>
-	<link rel="stylesheet" href="<?php echo get_bloginfo("template_url")?>/style.css" type="text/css" media="screen" charset="utf-8"0>
+	<link rel="stylesheet" href="<?php echo get_bloginfo("template_url")?>/bootstrap/css/bootstrap.min.css">
+	<link href="<?php echo get_bloginfo("template_url")?>/bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
+	<link rel="stylesheet" href="<?php echo get_bloginfo("template_url")?>/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="<?php echo get_bloginfo("template_url")?>/style.css">
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,400,300,700' rel='stylesheet' type='text/css'>
 	<title></title>
 </head>
 <body>
--- index.php
+++ index.php
@@ -5,6 +5,233 @@
 */
 get_header();
 ?>
-<h1> Hello, Thanh Son </h1>
+<header>
+  <div class="container">
+    <div class="row">
+      <div class="span6">
+        <div class="logo">
+          <h1><a href="#">Nguyen Sy <span class="color">Thanh Son</span></a></h1>
+          <div class="hmeta">Silence is golden!</div>
+        </div>
+      </div>
+      <div class="span6">
+        <div class="form">
+          <form method="get" id="searchform" action="#" class="form-search">
+            <input type="text" value="" name="s" id="s" class="input-medium">
+            <button type="submit" class="btn">Search</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</header>
+<div class="navbar">
+   <div class="navbar-inner">
+	 <div class="container">
+	   <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+		 <span>Menu</span>
+	   </a>
+	   <div class="nav-collapse collapse">
+		 <ul class="nav">
+		   <li><a href="index.html">Home #1</a></li>
+		   <li><a href="index1.html">Home #2</a></li>
+		   <!-- Refer Bootstrap navbar doc -->
+		   <li class="dropdown">
+			  <a href="#" class="dropdown-toggle" data-toggle="dropdown">Pages #1 <b class="caret"></b></a>
+			  <ul class="dropdown-menu">
+				<li><a href="landing-page.html">Landing Page</a></li>
+				<li><a href="pricing.html">Pricing Table</a></li>
+				<li><a href="service-3.html">Service</a></li>
+				<li><a href="support.html">Support</a></li>
+				<li><a href="sitemap.html">Sitemap</a></li>
+				<li><a href="timeline.html">Timeline</a></li>
+				<li><a href="404.html">404</a></li>
+				<li><a href="faq.html">FAQ</a></li>
+				<li><a href="register1.html">Register</a></li>
+				<li><a href="login1.html">Login</a></li>
+			  </ul>
+		   </li>                   
+		   <li class="dropdown">
+			  <a href="#" class="dropdown-toggle" data-toggle="dropdown">Pages #2<b class="caret"></b></a>
+			  <ul class="dropdown-menu">
+				<li><a href="coming-soon.html">Coming Soon</a></li>
+				<li><a href="features-4.html">Features</a></li>
+				<li><a href="statement.html">Statement</a></li>
+				<li><a href="tasks.html">Tasks</a></li>
+				<li><a href="resume.html">Resume</a></li>
+				<li><a href="projects.html">Projects</a></li>
+				<li><a href="make-post.html">Make Post</a></li>
+				<li><a href="events.html">Events</a></li>
+				<li><a href="error-log.html">Error Log</a></li>
+			  </ul>
+		   </li>
+		   <li><a href="service.html">Service</a></li>
+		   <li><a href="aboutus.html">About Us</a></li>
+		   <li class="dropdown">
+			  <a href="#" class="dropdown-toggle" data-toggle="dropdown">Blog <b class="caret"></b></a>
+			  <ul class="dropdown-menu">
+				<li><a href="blog-2.html">Blog #1</a></li>
+				<li><a href="blog-4.html">Blog #1</a></li>
+				<li><a href="blog-single.html">Blog Single</a></li>
+			  </ul>
+			</li>
+								
+		   <li><a href="portfolio.html">Portfolio</a></li>
+		   <li><a href="contactus.html">Contact</a></li>
+		 </ul>
+	   </div>
+	  </div>
+   </div>
+ </div>
+<div class="content">
+	<div class="container">
+		<div class="color-blocks">
+			<div class="row">
+			  <div class="span4">
+				<div class="c-block">
+				  <!-- Left column with color background -->
+				  <div class="col-l b-red">
+					<!-- Link -->
+					<a href="contactus.html">
+					  <!-- Icon -->
+					  <i class="icon-envelope-alt"></i>
+					  <!-- Text -->
+					  Contact
+					</a>
+				  </div>
+				  <div class="col-r b-purple">
+					<a href="aboutus.html">
+					  <i class="icon-user"></i>
+					  About
+					</a>
+				  </div>
+				</div>
+				<div class="clearfix"></div>
+			  </div>
+			  <div class="span4">
+				<div class="c-block">
+				  <!-- Left column with color background -->
+				  <div class="col-l b-green">
+					<!-- Link -->
+					<a href="blog.html">
+					  <!-- Icon -->
+					  <i class="icon-comments"></i>
+					  <!-- Text -->
+					  Blog
+					</a>
+				  </div>
+				  <div class="col-r b-blue">
+					<a href="#">
+					  <i class="icon-facebook"></i>
+					  Facebook
+					</a>
+				  </div>
+				</div>
+				<div class="clearfix"></div>
+			  </div>
+			  <div class="span4">
+				<div class="c-block">
+				  <!-- Left column with color background -->
+				  <div class="col-l b-lblue">
+					<!-- Link -->
+					<a href="#">
+					  <!-- Icon -->
+					  <i class="icon-twitter"></i>
+					  <!-- Text -->
+					  Twitter
+					</a>
+				  </div>
+				  <div class="col-r b-orange">
+					<a href="#">
+					  <i class="icon-google-plus"></i>
+					  Google Plus
+					</a>
+				  </div>
+				</div>
+				<div class="clearfix"></div>
+			  </div>
+			</div>
+		  </div>
+	</div>
+</div>
+<div class="social-links">
+  <div class="container">
+    <div class="row">
+      <div class="span12">
+        <p class="big"><span>Follow Us On</span> <a href="#"><i class="icon-facebook"></i>Facebook</a> <a href="#"><i class="icon-twitter"></i>Twitter</a> <a href="#"><i class="icon-google-plus"></i>Google Plus</a> <a href="#"><i class="icon-linkedin"></i>LinkedIn</a></p>
+      </div>
+    </div>    
+  </div>
+</div>
+<footer>
+  <div class="container">
+    <div class="row">
+
+      <div class="widgets">
+        <div class="span4">
+          <div class="fwidget">
+
+            <div class="col-l">
+              <h6>Downlaods</h6>
+              <ul>
+                <li><a href="#">Condimentum</a></li>
+                <li><a href="#">Etiam at</a></li>
+                <li><a href="#">Fusce vel</a></li>
+                <li><a href="#">Vivamus</a></li>
+                <li><a href="#">Pellentesque</a></li>
+              </ul>
+            </div>
+
+            <div class="col-r">
+              <h6>Support</h6>
+              <ul>
+                <li><a href="#">Condimentum</a></li>
+                <li><a href="#">Etiam at</a></li>
+                <li><a href="#">Fusce vel</a></li>
+                <li><a href="#">Vivamus</a></li>
+                <li><a href="#">Pellentesque</a></li>
+              </ul>
+            </div>
+
+          </div>
+        </div>
+
+        <div class="span4">
+          <div class="fwidget">
+            <h6>Categories</h6>
+            <ul>
+              <li><a href="#">Condimentum - Condimentum gravida</a></li>
+              <li><a href="#">Etiam at - Condimentum gravida</a></li>
+              <li><a href="#">Fusce vel - Condimentum gravida</a></li>
+              <li><a href="#">Vivamus - Condimentum gravida</a></li>
+              <li><a href="#">Pellentesque - Condimentum gravida</a></li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="span4">
+          <div class="fwidget">
+            <h6>Recent Posts</h6>
+            <ul>
+              <li><a href="#">Sed eu leo orci, condimentum gravida metus</a></li>
+              <li><a href="#">Etiam at nulla ipsum, in rhoncus purus</a></li>
+              <li><a href="#">Fusce vel magna faucibus felis dapibus facilisis</a></li>
+              <li><a href="#">Vivamus scelerisque dui in massa</a></li>
+              <li><a href="#">Pellentesque eget adipiscing dui semper</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div class="span12">
+          <div class="copy">
+            <h6>Metro <span class="color">Mania</span></h6>
+            <p>Copyright © <a href="#">Your Site</a> - <a href="index.html">Home</a> | <a href="aboutus.html">About Us</a> | <a href="faq.html">FAQ</a> | <a href="contactus.html">Contact Us</a></p>
+          </div>
+      </div>
+    </div>
+  <div class="clearfix"></div>
+  </div>
+</footer>
 <?php
 get_footer();
