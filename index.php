28aadc5f03bbb181804811c6f5b2f43209de3063
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2013-03-25 02:01:02 +0700

    index file

--- index.php
+++ index.php
@@ -83,8 +83,180 @@ get_header();
 	  </div>
    </div>
  </div>
+<div class="full-slider">
+	<!-- Slider (Flex Slider) -->
+	  <div class="container">
+		<div class="row">
+		  <div class="span12">
+			<div class="flexslider">
+			  <ul class="slides">
+				<li>
+				  <!-- Slider content -->
+				  <div class="flex-caption">
+					<!-- Left column -->
+					<div class="col-l">
+						<h2>What Do  Started Today</h2>
+						<h6>Aenean sodales augue ac lacus hendrerit sed rhoncus erat hendrerit. Vivamus vel ultricies elit. Nulla vitae cursus leo. Aenean sodales augue ac lacus hendrerit sed rhoncus erat hendrerit. Vivamus vel ultricies elit. Nulla vitae cursus leo.</h6>
+					</div>
+					<!-- Right column -->
+					<div class="col-r">
+					  <!-- Use the class "flex-back" to add background inside flex slider -->
+						<h5>Nulla Vitae Rhoncus</h5>
+						<p>Suspendisse potenti. Morbi ac felis nec mauris imperdiet fermentum. Aenean sodales augue ac lacus hendrerit sed rhoncus erat hendrerit. Vivamus vel ultricies elit. Nulla vitae cursus leo.</p>
+					  
+					  <!-- Button -->
+					  <div class="button">
+						<a href="#"><i class="icon-circle-arrow-down"></i> Download Our Software Today</a>
+					  </div>
+
+					</div>
+				  </div>
+
+				</li>
+
+				<li>
+				  <div class="flex-caption flex-center">
+					<h2>Don't Miss This Theme</h2>
+
+					<p>Aenean sodales augue ac lacus hendrerit sed rhoncus erat hendrerit. Vivamus vel ultricies elit. Nulla vitae cursus leo. Suspendisse potenti. Morbi ac felis nec mauris imperdiet fermentum. </p>
+
+					  <!-- Button -->
+					  <div class="button">
+						<a href="#">Buy It Now</a>
+					  </div>
+
+				  </div>
+				</li>
+				<li>
+				  <!-- Slider content -->
+				  <div class="flex-caption">
+					<!-- Left column -->
+					<div class="col-l">
+					  <h2>Get Started Today</h2>
+					  <h6>Suspendisse potenti. Morbi ac felis nec mauris imperdiet fermentum. Aenean sodales augue ac lacus hendrerit.</h6>
+
+					  <!-- Button -->
+					  <div class="button">
+						<a href="#"><i class="icon-circle-arrow-down"></i> Download</a>
+					  </div>
+
+					</div>
+					<!-- Right column -->
+					<div class="col-r">
+
+					  <!-- Use the class "flex-back" to add background inside flex slider -->
+					  <div class="flex-back">
+						<h5>Nulla Vitae Rhoncus</h5>
+						<p>Suspendisse potenti. Nulla vitae cursus leo. Morbi ac felis nec mauris imperdiet fermentum. Aenean sodales augue ac lacus hendrerit sed rhoncus erat hendrerit. Vivamus vel ultricies elit. Nulla vitae cursus leo.</p>
+					  </div>
+
+					</div>
+				  </div>
+
+				</li>
+				<li>
+
+				  <!-- Slider content -->
+				  <div class="flex-caption">
+					<!-- Left column -->
+					<div class="col-l">
+					  <h2>Someone Started Today</h2>
+					  <h6>Suspendisse potenti. Morbi ac felis nec mauris imperdiet fermentum. Aenean sodales augue ac lacus hendrerit sed rhoncus erat hendrerit. Vivamus vel ultricies elit.Vivamus vel ultricies elit.</h6>
+					</div>
+					<!-- Right column -->
+					<div class="col-r">
+
+					  <!-- Use the class "flex-back" to add background inside flex slider -->
+					  
+						<h2>Nulla Vitae Rhoncus</h2>
+						<p>Suspendisse potenti. Morbi ac felis nec mauris imperdiet fermentum. Aenean sodales augue ac lacus hendrerit sed rhoncus erat hendrerit. Vivamus vel ultricies elit. Nulla vitae cursus leo.</p>
+					</div>
+				  </div>
+				</li>
+			  </ul>
+			<ol class="flex-control-nav flex-control-paging"><li><a class="">1</a></li><li><a class="">2</a></li><li><a class="flex-active">3</a></li><li><a class="">4</a></li></ol></div>
+		  </div>
+		</div>
+	  </div>
+</div>
 <div class="content">
 	<div class="container">
+		<div class="row center">
+		  <div class="span12">
+			<div class="bor"></div>
+			<h2>Welcome to Metro Mania</h2>
+			<p>Suspendisse potenti. Morbi ac felis nec mauris imperdiet fermentum. Aenean sodales augue ac lacus hendrerit sed rhoncus erat hendrerit. Vivamus vel ultricies elit.</p>
+			<div class="bor"></div>
+			<hr>
+		  </div>  
+		</div>
+		<div class="row">
+			<div class="span6">
+			   <!-- Timeline #1 -->
+			   <div class="time">
+				  <div class="tidate b-red">
+					 1990
+				  </div>
+				  <div class="timatter">
+					 <h5>Nulla ullamcorper</h5>
+					 <p>Praesent at tellus porttitor nisl porttitor sagittis. Mauris in massa ligula, a tempor nulla. Ut tempus interdum mauris vel vehicula. Nulla ullamcorper tortor commodo in sagittis est accumsan.</p>
+				  </div>
+				  <div class="clearfix"></div>
+			   </div>
+			   <div class="time">
+				  <div class="tidate b-blue">
+					 1994
+				  </div>
+				  <div class="timatter">
+					 <h5>Nulla ullamcorper</h5>
+					 <p>Praesent at tellus porttitor nisl porttitor sagittis. Mauris in massa ligula, a tempor nulla. Ut tempus interdum mauris vel vehicula. Nulla ullamcorper tortor commodo in sagittis est accumsan.</p>
+				  </div>
+				  <div class="clearfix"></div>
+			   </div>
+			   <div class="time">
+				  <div class="tidate b-green">
+					 1996
+				  </div>
+				  <div class="timatter">
+					 <h5>Nulla ullamcorper</h5>
+					 <p>Praesent at tellus porttitor nisl porttitor sagittis. Mauris in massa ligula, a tempor nulla. Ut tempus interdum mauris vel vehicula. Nulla ullamcorper tortor commodo in sagittis est accumsan.</p>
+				  </div>
+				  <div class="clearfix"></div>
+			   </div>
+			</div>                        
+			<div class="span6">
+			   <div class="time">
+				  <div class="tidate b-purple">
+					 1992
+				  </div>
+				  <div class="timatter">
+					 <h5>Nulla ullamcorper</h5>
+					 <p>Praesent at tellus porttitor nisl porttitor sagittis. Mauris in massa ligula, a tempor nulla. Ut tempus interdum mauris vel vehicula. Nulla ullamcorper tortor commodo in sagittis est accumsan.</p>
+				  </div>
+				  <div class="clearfix"></div>
+			   </div>       
+			   <div class="time">
+				  <div class="tidate b-orange">
+					 1999
+				  </div>
+				  <div class="timatter">
+					 <h5>Nulla ullamcorper</h5>
+					 <p>Praesent at tellus porttitor nisl porttitor sagittis. Mauris in massa ligula, a tempor nulla. Ut tempus interdum mauris vel vehicula. Nulla ullamcorper tortor commodo in sagittis est accumsan.</p>
+				  </div>
+				  <div class="clearfix"></div>
+			   </div>
+			   <div class="time">
+				  <div class="tidate b-lblue">
+					 2000
+				  </div>
+				  <div class="timatter">
+					 <h5>Nulla ullamcorper</h5>
+					 <p>Praesent at tellus porttitor nisl porttitor sagittis. Mauris in massa ligula, a tempor nulla. Ut tempus interdum mauris vel vehicula. Nulla ullamcorper tortor commodo in sagittis est accumsan.</p>
+				  </div>
+				  <div class="clearfix"></div>
+			   </div>
+			</div>
+		 </div>
 		<div class="color-blocks">
 			<div class="row">
 			  <div class="span4">
