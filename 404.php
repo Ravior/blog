258e7318f2106b7f6dc8d600348cff13943e9729
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2013-03-27 14:41:51 +0700

    category page

---
+++ 404.php
@@ -0,0 +1,13 @@
+<?php
+get_header();
+?>
+<div class="content">
+  <div class="container">
+
+  <h2>Oops!!! Its 404!</h2>
+  <p class="big grey">We are sorry, the page you requested cannot be found. </p>
+
+  </div>
+</div>
+<?php
+get_footer();
--- header.php
+++ header.php
@@ -8,12 +8,12 @@
 <head>
 	<meta http-equiv="content-type" content="text/html; charset=utf-8"0>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,400,300,700' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" href="<?php echo get_bloginfo("template_url")?>/bootstrap/css/bootstrap.min.css">
-	<link href="<?php echo get_bloginfo("template_url")?>/bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
 	<link rel="stylesheet" href="<?php echo get_bloginfo("template_url")?>/font-awesome/css/font-awesome.min.css">
 	<link rel="stylesheet" href="<?php echo get_bloginfo("template_url")?>/css/flexslider.css">
 	<link rel="stylesheet" href="<?php echo get_bloginfo("template_url")?>/style.css">
-	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,400,300,700' rel='stylesheet' type='text/css'>
+	<link href="<?php echo get_bloginfo("template_url")?>/bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
 	<title></title>
 </head>
 <body>
